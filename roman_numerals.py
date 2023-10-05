362f43739c363530ca0c6d1cf18d244618111e06
Megan Buck <thequiltingriverotter@gmail.com>
2023-10-05 18:33:46 -0400

    Added basic for loop, map, and empty string

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,3 +1,13 @@
 def to_roman(num):
-    # write your code here!
-    pass
+    output = ""
+    ROMAN_NUMERAL_TO_ARABIC_MAP = {
+        "M": 1000,
+        "D": 500,
+        "C": 100,
+        "L": 50,
+        "X": 10,
+        "V": 5,
+        "I": 1,
+    }
+    for numeral, number in ROMAN_NUMERAL_TO_ARABIC_MAP.entries():
+        print(numeral, number)
